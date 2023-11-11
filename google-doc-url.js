db50807f11113f7467c96934cf2969d955e1a991
andrzeyka <150247794+andrzeyka@users.noreply.github.com>
2023-11-11 23:21:17 +0100

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1GsNlFCBUPGrRXweL7TYHu97yPuzqCdNTkyhXxzCydf8/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1GsNlFCBUPGrRXweL7TYHu97yPuzqCdNTkyhXxzCydf8/edit?usp=sharing';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = ' https://andrzeyka.github.io/andrei/';
